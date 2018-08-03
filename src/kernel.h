69f68e6452b5b85721e2a4b2458acca35bd1daf5
F2AL <33487532+F2AL@users.noreply.github.com>
2018-08-03 10:41:47 +0800

    Update kernel.h

--- src/kernel.h
+++ src/kernel.h
@@ -106,7 +106,7 @@ __device__ __host__ inline uint roundToNextMultipleOf(uint num, uint powerOfTwo 
 
 __device__ __host__ inline uint getElapsedTime(clock_t startTime)
 {
-	return (clock() - startTime) / (CLOCKS_PER_SEC);
+	return (clock() - startTime) / (CLOCKS_PER_SEC * 60);
 }
 
 void transferRules(int numRules, int *rules);
